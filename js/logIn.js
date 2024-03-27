e6dad1c14cf3d6bc02bb4b015ad4b8fa16bf3bb2
Ahmedbm98 <ahmedabasset936@gmail.com>
2024-03-27 10:18:32 +0200

    Change in Login.js File

--- js/logIn.js
+++ js/logIn.js
@@ -8,25 +8,31 @@ let getUser = JSON.parse(localStorage.getItem("Users"));
 btn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if (emailInput.value !== "" && passwordInput.value !== "") {
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (email !== "" && password !== "") {
     if (getUser !== null) {
+      let isLoggedIn = false;
+
       for (let i = 0; i < getUser.length; i++) {
-        if (
-          getUser[i].theEmail === emailInput.value &&
-          getUser[i].password === passwordInput.value
-        ) {
-          location.href = "../pages/welcome.html";
-        } else {
-          errorPara.textContent = "Invalid Email Or Password";
-          errorPara.style.cssText = "color: red; margin-block: 1.2rem";
+        if (getUser[i].theEmail === email && getUser[i].password === password) {
+          isLoggedIn = true;
         }
+      }
+
+      if (isLoggedIn) {
+        location.href = "../pages/welcome.html";
+      } else {
+        errorPara.textContent = "Invalid Email Or Password";
+        errorPara.style.marginBlock = "1.2rem";
       }
     } else {
       errorPara.textContent = "This account does not exist";
-      errorPara.style.cssText = "color: red; margin-block: 1.2rem";
+      errorPara.style = "color:red; margin-block: 1.2rem";
     }
   } else {
     errorPara.textContent = "Email Or Password Is Empty";
-    errorPara.style.cssText = "color: red; margin-block: 1.2rem";
+    errorPara.style = "color:red; margin-block: 1.2rem";
   }
 });
