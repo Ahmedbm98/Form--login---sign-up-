68cec1521a86b557cd3c9b4b1bd55d4445f6ca43
Ahmedbm98 <ahmedabasset936@gmail.com>
2024-03-28 10:54:37 +0200

    update

--- js/logIn.js
+++ js/logIn.js
@@ -8,8 +8,8 @@ let getUser = JSON.parse(localStorage.getItem("Users"));
 btn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+  let email = emailInput.value.trim();
+  let password = passwordInput.value.trim();
 
   if (email === "" || password === "") {
     errorPara.textContent = "Email Or Password Is Empty";
@@ -17,45 +17,15 @@ btn.addEventListener("click", function (e) {
     return;
   }
 
-  let userFound = false;
   if (getUser !== null) {
     for (let i = 0; i < getUser.length; i++) {
       if (getUser[i].theEmail === email && getUser[i].password === password) {
-        userFound = true;
         location.href = "../pages/welcome.html";
         break;
+      } else {
+        errorPara.textContent = "Invalid Password Or Email";
+        errorPara.style.cssText = "color:red; margin-block: 1.2rem";
       }
     }
   }
-
-  if (!userFound) {
-    errorPara.textContent = "Invalid Password Or Email";
-    errorPara.style.cssText = "color:red; margin-block: 1.2rem";
-  }
-
-  // if (email !== "" && password !== "") {
-  //   if (getUser !== null) {
-  //     let isLoggedIn = false;
-
-  //     for (let i = 0; i < getUser.length; i++) {
-  //       if (getUser[i].theEmail === email && getUser[i].password === password) {
-  //         isLoggedIn = true;
-  //         break;
-  //       }
-  //     }
-
-  //     if (isLoggedIn === true) {
-  //       location.replace("../pages/welcome.html");
-  //     } else {
-  //       errorPara.textContent = "Invalid Email Or Password";
-  //       errorPara.style.marginBlock = "1.2rem";
-  //     }
-  //   } else {
-  //     errorPara.textContent = "This account does not exist";
-  //     errorPara.style = "color:red; margin-block: 1.2rem";
-  //   }
-  // } else {
-  //   errorPara.textContent = "Email Or Password Is Empty";
-  //   errorPara.style = "color:red; margin-block: 1.2rem";
-  // }
 });
