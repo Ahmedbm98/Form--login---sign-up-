effad0da044518e2dec7c931fd1d667f31a1d1d3
Ahmedbm98 <ahmedabasset936@gmail.com>
2024-03-28 02:19:24 +0200

    update

--- js/logIn.js
+++ js/logIn.js
@@ -1,6 +1,6 @@
 let emailInput = document.querySelector("input[type='email']");
 let passwordInput = document.querySelector("input[type='password']");
-let btn = document.querySelector("a");
+let btn = document.querySelector("button");
 let errorPara = document.getElementById("error");
 
 let getUser = JSON.parse(localStorage.getItem("Users"));
@@ -29,4 +29,30 @@ btn.addEventListener("click", function (e) {
     errorPara.textContent = "Email Or Password Is Empty";
     errorPara.style = "color:red; margin-block: 1.2rem";
   }
+
+  // if (email !== "" && password !== "") {
+  //   if (getUser !== null) {
+  //     let isLoggedIn = false;
+
+  //     for (let i = 0; i < getUser.length; i++) {
+  //       if (getUser[i].theEmail === email && getUser[i].password === password) {
+  //         isLoggedIn = true;
+  //         break;
+  //       }
+  //     }
+
+  //     if (isLoggedIn === true) {
+  //       location.replace("../pages/welcome.html");
+  //     } else {
+  //       errorPara.textContent = "Invalid Email Or Password";
+  //       errorPara.style.marginBlock = "1.2rem";
+  //     }
+  //   } else {
+  //     errorPara.textContent = "This account does not exist";
+  //     errorPara.style = "color:red; margin-block: 1.2rem";
+  //   }
+  // } else {
+  //   errorPara.textContent = "Email Or Password Is Empty";
+  //   errorPara.style = "color:red; margin-block: 1.2rem";
+  // }
 });
