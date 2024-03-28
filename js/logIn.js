a7c186933ab6bc38f70cc5a6e5bab3190da7d86c
Ahmedbm98 <ahmedabasset936@gmail.com>
2024-03-28 02:06:52 +0200

    update

--- js/logIn.js
+++ js/logIn.js
@@ -13,20 +13,13 @@ btn.addEventListener("click", function (e) {
 
   if (email !== "" && password !== "") {
     if (getUser !== null) {
-      let isLoggedIn = false;
-
       for (let i = 0; i < getUser.length; i++) {
         if (getUser[i].theEmail === email && getUser[i].password === password) {
-          isLoggedIn = true;
-          break;
+          location.href = "../pages/welcome.html";
+        } else {
+          errorPara.textContent = "Invalid Email Or Password";
+          errorPara.style = "color:red; margin-block: 1.2rem";
         }
-      }
-
-      if (isLoggedIn === true) {
-        location.replace("../pages/welcome.html");
-      } else {
-        errorPara.textContent = "Invalid Email Or Password";
-        errorPara.style.marginBlock = "1.2rem";
       }
     } else {
       errorPara.textContent = "This account does not exist";
@@ -36,4 +29,30 @@ btn.addEventListener("click", function (e) {
     errorPara.textContent = "Email Or Password Is Empty";
     errorPara.style = "color:red; margin-block: 1.2rem";
   }
+
+  // if (email !== "" && password !== "") {
+  //   if (getUser !== null) {
+  //     let isLoggedIn = false;
+
+  //     for (let i = 0; i < getUser.length; i++) {
+  //       if (getUser[i].theEmail === email && getUser[i].password === password) {
+  //         isLoggedIn = true;
+  //         break;
+  //       }
+  //     }
+
+  //     if (isLoggedIn === true) {
+  //       location.replace("../pages/welcome.html");
+  //     } else {
+  //       errorPara.textContent = "Invalid Email Or Password";
+  //       errorPara.style.marginBlock = "1.2rem";
+  //     }
+  //   } else {
+  //     errorPara.textContent = "This account does not exist";
+  //     errorPara.style = "color:red; margin-block: 1.2rem";
+  //   }
+  // } else {
+  //   errorPara.textContent = "Email Or Password Is Empty";
+  //   errorPara.style = "color:red; margin-block: 1.2rem";
+  // }
 });
